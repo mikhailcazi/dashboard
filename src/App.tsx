02076349df5a824aa15eeb3d01e25c11e4436b10
Mikhail <mikhailcazi@gmail.com>
2025-05-31 23:40:45 +0530

    Adding routing, changing font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
+import Page from "./app/dashboard/page";
 import Header from "./components/header";
 
 function App() {
   return (
     <>
       <div className="flex h-screen flex-col">
-        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Page />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
